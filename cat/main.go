5f33380796afb50d9dee7c2e62039a92f74ba675
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-07-27 15:11:10 -0700

    Uses go fmt

--- cat/main.go
+++ cat/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
 )
 
 func cat(filename string) {
-  contents, err := ioutil.ReadFile(filename)
-  if err != nil {
-    panic(err)
-  }
-  fmt.Printf("%s", contents)
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", contents)
 }
 
 func main() {

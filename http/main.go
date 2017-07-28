670cfefa379f024b052dc6778754bdd1aefe632a
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-07-28 14:40:04 -0700

    http measuring things

---
+++ http/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+	"net/http"
+	"sync"
+  "time"
+)
+
+func query() {
+  timeNow := time.Now()
+  resp, err := http.Get("http://google.com")
+
+  if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+
+  fmt.Printf("Status: %s, Response Time: %s\n", resp.Status, time.Since(timeNow))
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			query()
+		}()
+	}
+	wg.Wait()
+}
